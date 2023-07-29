71cf2782f1f79d57e15ff0e3c16f093527c7b70f
Albert Arques <albertarques@gmail.com>
2023-07-29 09:06:38 +0200

    Reorganized css styles and menu size adapt

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -2,15 +2,24 @@ import Link from "next/link";
 import Navbar from "../nav/Navbar";
 
 export default function Header() {
-  const sm = ``;
-  const md = ``;
-  const lg = ``;
+
+  const header = {
+    def : "flex fixed px-4 my-3 justify-between w-screen align-middle h-[2rem]",
+    md: "md:h-[3rem]",
+    lg: "lg:h-[4rem]",
+  }
+  
+  const h1 = {
+    def : "text-[2rem] flex font-bold text-transparent text-center z-20 golden-animation bg-clip-text align-middle items-center h-[100%]",
+    md : "md:text-[3rem] ",
+    lg: "lg:text-[4rem]",
+  }
 
   return (
     <>
-      <header className="flex fixed px-4 my-3 justify-between w-screen align-middle h-[5rem]">
+      <header className={`${header.def} ${header.md}`}>
         <Link href="/">
-          <h1 className="text-[5rem] flex font-bold text-transparent text-center z-20 golden-animation bg-clip-text align-middle items-center h-[100%]">
+          <h1 className={`${h1.def} ${h1.md}`}>
             AAD
           </h1>
         </Link>
--- src/app/components/nav/Navbar.jsx
+++ src/app/components/nav/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar({children}) {
       <a
         href={url}
         key={title}
-        className="flex rounded-lg text-gray-900 font-bold hover:text-gray-700 hover:text-transparent align-middle items-center text-[2rem] golden-animation bg-clip-text"
+        className="flex rounded-lg text-gray-900 font-bold hover:text-gray-700 hover:text-transparent align-middle items-center golden-animation bg-clip-text"
       >
         {title}
       </a>
